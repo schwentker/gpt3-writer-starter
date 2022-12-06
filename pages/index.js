d64fa219106fb916c93729cf7c7d14c89dc87cab
Robert Schwentker <schwentker@gmail.com>
2022-12-05 22:27:47 -0800

    update index generate etc

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,61 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
+  const onUserChangedText = (event) => {
+  // console.log(event.target.value);
+  setUserInput(event.target.value);
+};
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Create tweet thread from YouTube transcript</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Select a YouTube with a transcript & click Generate</h2>
           </div>
         </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput}
+  onChange={onUserChangedText}/>
+  <div className="prompt-buttons">
+
+<a className="generate-button" onClick={callGenerateEndpoint}>
+  <div className="generate">
+    <p>Generate</p>
+  </div>
+</a>
+</div>
+  </div>
       </div>
       <div className="badge-container grow">
         <a
